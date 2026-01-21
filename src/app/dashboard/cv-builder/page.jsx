b7cea86d76d25db75ae769ea782cffe4a238d912
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-01-21 18:18:00 +0600

    title and cv completion done

---
+++ src/app/dashboard/cv-builder/page.jsx
@@ -0,0 +1,11 @@
+import TitleProgress from "./_components/title-progress";
+
+const page = () => {
+  return (
+    <div className="p-4">
+      <TitleProgress />
+    </div>
+  );
+};
+
+export default page;
