3425c09b115b099ff209ddb09739b9ea2d1f6aad
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2026-02-04 20:33:47 +0600

    add

--- src/app/(students)/dashboard/portfolio/page.tsx
+++ src/app/(students)/dashboard/portfolio/page.tsx
@@ -1,11 +1,12 @@
+import StudentNavbar from "@/components/shared/StudentNavbar";
 import PortfolioEvents from "./_components/portfolio-events";
 import PortfolioHero from "./_components/portfolio-hero";
 
 export default function PortfolioPage() {
     return (
         <div className="bg-[#FEFDF6]">
-            <PortfolioHero/>
-            <PortfolioEvents/>
+            <PortfolioHero />
+            <PortfolioEvents />
         </div>
     );
 }
