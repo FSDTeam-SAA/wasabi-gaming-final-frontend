e05d7fce20a5aa61c823dbf5b5d2dcb9257d8cec
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2026-02-10 11:30:01 +0600

    update SchoolAboutTitle component to change placeholder text

--- src/components/about/school/SchoolAboutTitle.jsx
+++ src/components/about/school/SchoolAboutTitle.jsx
@@ -5,10 +5,7 @@ const SchoolAboutTitle = () => {
         About <span className="text-[#EDCC13]">Aspiring</span>
       </h1>
       <p className="text-[#5A5A5A] text-lg lg:text-2xl">
-        Aspiring helps you create professional, job-ready resumes in just a few
-        minutes. Build, customize, and share your CV effortlessly with our
-        easy-to-use tools, designed to help you showcase your skills, stand out
-        to employers, and take the next big step in your career.
+        monirrrrrr
       </p>
     </div>
   );
