482501400d557df823955649e4622b7f1e7fc451
amitchackroborty04 <amitck803@gmail.com>
2026-03-02 14:21:34 +0600

    courses section fixed

--- src/app/(students)/dashboard/courses/_components/all-course.tsx
+++ src/app/(students)/dashboard/courses/_components/all-course.tsx
@@ -116,7 +116,6 @@ export default function AllCourse() {
                         ))}
                     </div>
                 </section>
-                {/* COURSES */}
                 <CourseCard />
             </main>
         </div>
